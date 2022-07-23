2c1baa539dc0775d2a2df27c76a3c819259aa7ce
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2022-07-23 17:36:10 -0600

    Added terminology to research section.

--- mathsite/main/static/js/script.js
+++ mathsite/main/static/js/script.js
@@ -1,6 +1,10 @@
 // Source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_filter
 // USED FOR THE DROPDOWN SEARCH BAR 
 
+////////////////////////////////////////////////////////////////////////////////
+// script.js IS NOT BEING LOADED PROPERLY, THIS FILE IS CURRENTLY NOT WORKING //
+////////////////////////////////////////////////////////////////////////////////
+
 
 // Source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup
 // USED FOR THE POPUP IN THE TOP RIGHT CORNER
